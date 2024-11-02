98bd058d70cd068fcad528413c917e65c54e0044
Soto <juanmiguelsoto4@gmail.com>
2024-11-02 00:24:42 -0500

    Finalizacion codigo

--- src/components/Cart/CartFooter.js
+++ src/components/Cart/CartFooter.js
@@ -1,4 +1,4 @@
-import React, {useContext, useMemo} from 'react';
+import React, {useContext} from 'react';
 import {View, Text} from 'react-native';
 import {Button} from '@rneui/themed';
 import {useNavigation} from '@react-navigation/native';
@@ -7,16 +7,8 @@ import CartStyles from '../../styles/screens/CartStyles';
 import {ProductContext} from '../../context/ProductContext';
 
 const CartFooter = () => {
-  const {cart, products} = useContext(ProductContext);
+  const {cartSubtotal} = useContext(ProductContext);
   const navigation = useNavigation();
-
-  const subtotal = useMemo(() => {
-    return Object.values(cart).reduce((total, item) => {
-      const product = products.find(p => p.id === item.product);
-      const price = product?.onOffer ? product.offerPrice : product?.price;
-      return total + (price || 0) * item.quantity;
-    }, 0);
-  }, [cart, products]);
 
   const handleCheckout = () => {
     navigation.navigate('Payment');
@@ -26,7 +18,9 @@ const CartFooter = () => {
     <View style={CartStyles.footerContainer}>
       <View style={CartStyles.subtotalContainer}>
         <Text style={CartStyles.subtotalText}>Subtotal</Text>
-        <Text style={CartStyles.subtotalPrice}>US$ {subtotal.toFixed(2)}</Text>
+        <Text style={CartStyles.subtotalPrice}>
+          US$ {cartSubtotal.toFixed(2)}
+        </Text>
       </View>
       <Button
         title="Continue to checkout"
